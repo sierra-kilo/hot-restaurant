39467b682c6e3e9909453daf94336701c26311a8
sierra-kilo <tashnag069@yahoo.com>
2017-10-12 21:14:54 -0700

    Saks branch1 (#3)
    * updated server and stuff
    
    * Tom is amazing

---
+++ app.js
@@ -0,0 +1,36 @@
+function Reservation(name, email, phone, id) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.id = id;
+}
+
+$("form").submit(function(event) {
+    event.preventDefault();
+
+    var customerInfo = {
+        customerName: $("#name").val().trim(),
+        phoneNumber: $("#email").val().trim(),
+        customerEmail: $("#phone").val().trim(),
+        customerID: $("#id").val().trim()
+    };
+
+})
+
+
+
+
+
+
+
+
+
+
+// get info from front end at submit button
+// create new instance of reservations
+// add instance to the list
+
+// for loop with a counter, that takes the first 5 reservations and adds it to tableList and takes all other reservations, and adds it to waitList
+
+
+//module.exports = *;
